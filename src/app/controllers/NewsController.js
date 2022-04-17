f5380b90dd5cf6528bf0c9b73afc85f3218cad78
tudda <tentufancr7@gmail.com>
2022-04-17 18:13:55 +0700

    router/controller

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,13 @@
+const res = require("express/lib/response");
+
+class NewsController {
+  //GET /news
+  index(req, res) {
+    res.render("news");
+  }
+  //GET /news/slug
+  show(req, res) {
+    res.send("news detail");
+  }
+}
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ const morgan = require("morgan");
 const app = express();
 const path = require("path");
 const hbs = require("express-handlebars");
+// middleware for post method and json
+const route = require("./routes");
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
+
 //http loger
 app.use(morgan("combined"));
 // template engine
@@ -18,12 +28,9 @@ app.engine(
 app.set("view engine", "hbs");
 
 app.set("views", path.join(__dirname, "resource", "views"));
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+
+route(app);
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:3000/`);
 });
